de04def105707c30458112d4f89c069f65557f5d
sbr-vincent <vincent.g.sbr@gmail.com>
2023-01-05 11:32:54 -0800

    Created a 2nd example for useRef

--- src/components/UseRefExample2.jsx
+++ src/components/UseRefExample2.jsx
@@ -1,8 +1,22 @@
 import { useState, useEffect, useRef } from "react"
-
+// Set a ref to a specific number of renders. Doesn't need to be set to DOM elements
 function UseRefExample2() {
+    const [name, setName] = useState("")
+    const renders = useRef(1)
+    // Used to get the previous state
+    const prevName = useRef("")
+
+    useEffect(() => {
+        renders.current = renders.current + 1
+        prevName.current = name
+    }, [name])
+
     return (
-        <div>UseRefExample2</div>
+        <div>
+            <h1>Renders: {renders.current}</h1>
+            <h2>Prev Name State: {prevName.current}</h2>
+            <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control mb-3" />
+        </div>
     )
 }
 
